f4753afd0ff9008fb153ce5f8620940404237d0a
565061763 <565061763@qq.com>
2022-12-02 14:26:50 +0800

    api

--- lib/src/main/java/com/sd/lib/compose/layer/Layer.kt
+++ lib/src/main/java/com/sd/lib/compose/layer/Layer.kt
@@ -1,88 +1,10 @@
 package com.sd.lib.compose.layer
 
 import android.util.Log
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxScope
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.IntSize
-
-/**
- * 用来存放Layer的容器
- */
-@Composable
-fun LayerContainer(
-    modifier: Modifier = Modifier,
-    content: @Composable BoxScope.() -> Unit,
-) {
-    val layerManager = remember { LayerManager() }
-    CompositionLocalProvider(LocalLayerManager provides layerManager) {
-        Box(
-            modifier = modifier
-                .fillMaxSize()
-                .onGloballyPositioned {
-                    layerManager.updateContainerLayout(it)
-                },
-            contentAlignment = Alignment.Center,
-        ) {
-            content()
-            layerManager.Layers()
-        }
-    }
-}
-
-/**
- * 创建并记住[Layer]
- */
-@Composable
-fun rememberLayer(): Layer {
-    val layerManager = checkNotNull(LocalLayerManager.current) {
-        "CompositionLocal LocalLayerManager not present"
-    }
-    return layerManager.rememberLayer()
-}
-
-/**
- * 创建并记住[TargetLayer]
- */
-@Composable
-fun rememberTargetLayer(): TargetLayer {
-    val layerManager = checkNotNull(LocalLayerManager.current) {
-        "CompositionLocal LocalLayerManager not present"
-    }
-    return layerManager.rememberTargetLayer()
-}
-
-/**
- * 设置要对齐的目标并绑定唯一的[tag]
- */
-fun Modifier.layerTarget(
-    tag: String,
-) = composed {
-    val layerManager = checkNotNull(LocalLayerManager.current) {
-        "CompositionLocal LocalLayerManager not present"
-    }
-
-    DisposableEffect(layerManager, tag) {
-        onDispose {
-            layerManager.removeTarget(tag)
-        }
-    }
-
-    this.onGloballyPositioned {
-        layerManager.addTarget(tag, it)
-    }
-}
-
 
 interface Layer {
     /**
