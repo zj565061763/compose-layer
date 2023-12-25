3b0a4005c84c39848394b10c6fc5f2cf693432e2
zjun <565061763@qq.com>
2023-12-25 16:25:35 +0800

    modifier

--- lib/src/main/java/com/sd/lib/compose/layer/Display.kt
+++ lib/src/main/java/com/sd/lib/compose/layer/Display.kt
@@ -8,20 +8,27 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 internal val DefaultDisplay: @Composable LayerDisplayScope.() -> Unit = { DisplayDefault() }
 
 @Composable
-fun LayerDisplayScope.DisplaySlideUpDown() {
+fun LayerDisplayScope.DisplaySlideUpDown(
+    modifier: Modifier = Modifier,
+) {
     DisplayDefault(
+        modifier = modifier,
         enter = slideInVertically { it },
         exit = slideOutVertically { it },
     )
 }
 
 @Composable
-fun LayerDisplayScope.DisplaySlideDownUp() {
+fun LayerDisplayScope.DisplaySlideDownUp(
+    modifier: Modifier = Modifier,
+) {
     DisplayDefault(
+        modifier = modifier,
         enter = slideInVertically { -it },
         exit = slideOutVertically { -it },
     )
@@ -29,10 +36,12 @@ fun LayerDisplayScope.DisplaySlideDownUp() {
 
 @Composable
 fun LayerDisplayScope.DisplayDefault(
+    modifier: Modifier = Modifier,
     enter: EnterTransition = fadeIn(),
     exit: ExitTransition = fadeOut(),
 ) {
     AnimatedVisibility(
+        modifier = modifier,
         visible = layer.isVisibleState,
         enter = enter,
         exit = exit,
