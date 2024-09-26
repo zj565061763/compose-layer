ed2d146ea4da166e0e59398c34b9a7849414b047
zjun <565061763@qq.com>
2024-09-26 17:47:48 +0800

    1.5.0-alpha05

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "compose-layer"
-val libVersion = "1.5.0-alpha04"
+val libVersion = "1.5.0-alpha05"
 
 android {
    namespace = "com.sd.lib.compose.layer"
