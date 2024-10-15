b26d27c12fc7ec735b0d3c0936e762992c1f7eb3
zjun <565061763@qq.com>
2024-10-15 21:18:37 +0800

    preview

--- app/src/main/java/com/sd/demo/compose_layer/SampleDropDown.kt
+++ app/src/main/java/com/sd/demo/compose_layer/SampleDropDown.kt
@@ -18,6 +18,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sd.demo.compose_layer.ui.theme.AppTheme
 import com.sd.lib.compose.layer.Directions
@@ -73,5 +74,13 @@ private fun Content() {
          count = 5,
          modifier = Modifier.width(200.dp),
       )
+   }
+}
+
+@Preview
+@Composable
+private fun PreviewContent() {
+   LayerContainer {
+      Content()
    }
 }
