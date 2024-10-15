b593598504f215987b32fe1201d9877a1c0f417e
zjun <565061763@qq.com>
2024-10-15 21:17:28 +0800

    refactor: remove fillMaxSize()

--- lib/src/main/java/com/sd/lib/compose/layer/Api.kt
+++ lib/src/main/java/com/sd/lib/compose/layer/Api.kt
@@ -1,7 +1,6 @@
 package com.sd.lib.compose.layer
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.DisposableEffect
@@ -37,7 +36,6 @@ fun LayerContainer(
    ) {
       Box(
          modifier = modifier
-            .fillMaxSize()
             .onGloballyPositioned {
                container.updateContainerLayout(it)
             },
